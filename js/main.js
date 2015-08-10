10d0639380729d8c156351edfb2ef142ff25cb5f
chrisyoong1self <chris.yoong.1self@gmail.com>
2015-08-10 10:57:22 +0100

    fix bottom menu desktop

--- js/main.js
+++ js/main.js
@@ -34,6 +34,18 @@ $(document).ready(function(){
   });
 
 
+  $(".flyout").find("a").click(function (e) {
+    e.preventDefault();                   // prevent default anchor behavior
+    var goTo = this.getAttribute("href"); // store anchor href
+
+    // do something while timeOut ticks ... 
+
+    setTimeout(function(){
+         window.location = goTo;
+    }, 350);       
+}); 
+
+
 // var backOfCard = $('.back').first().detach();
 
 // $(".more").click(function(){
